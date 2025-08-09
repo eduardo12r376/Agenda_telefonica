fa0bbf6fff2ef66bdf94eac36b79ac01a7b4265d
Eduardo hdrz <eduardomgir@gmail.com>
2025-08-08 21:15:55 -0600

    3.5: Backend de la Agenda Telefónica, paso 5

--- index.js
+++ index.js
@@ -68,6 +68,27 @@ app.delete('/api/persons/:id', (request, response) =>{
     response.status(204).end();
 })
 
+const generateId = () => {
+    const maxId = persons.length > 0 
+    ? Math.max(...persons.map(p => p.id)) : 0;
+
+    return maxId +1;
+}
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body;
+
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: generateId()
+    }
+
+    persons = persons.concat(person);
+    response.json(person);
+})
+
+
 const PORT = 3002;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
